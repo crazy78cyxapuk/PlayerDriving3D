1f8f220c3aa476dabdcd8dacecd3dcee8d4b7fed
crazy78cyxapuk <olezhki.net@gmail.com>
2020-11-07 18:13:00 +0300

    сделано управление игрока

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float powerJump;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Move(false);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Move(true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            StopMove();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(CheckGround());
+            if (CheckGround())
+            {
+                Jump();
+            }
+        }
+    }
+
+    private void StopMove()
+    {
+        rb.velocity = Vector3.zero;
+    }
+
+    private void Move(bool isRight)
+    {
+        if (isRight)
+        {
+            //Right
+            rb.AddForce(transform.right * speed * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        else
+        {
+            //Left
+            rb.AddForce(-transform.right * speed * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        Debug.Log(rb.velocity);
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(transform.up * powerJump, ForceMode.Impulse);
+    }
+
+    private bool CheckGround()
+    {
+        Vector3 checkDown = transform.TransformDirection(Vector3.down);
+
+        if (Physics.Raycast(transform.position, checkDown, 1))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
